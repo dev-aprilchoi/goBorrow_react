875a98643bd47faaa0f400a92fd32ba414f896e8
sjkang930 <91238446+sjkang930@users.noreply.github.com>
2023-09-27 23:00:08 +0900

    deleted img file

--- src/component/common/sidebar.js
+++ src/component/common/sidebar.js
@@ -1,27 +1,38 @@
 import styled from '@emotion/styled';
 
 const Sidebar = styled.div`
-    background-color: red;
+    /* background-color: red;
     color: blue;
     position: absolute;
     right: 0;
     top: 102px;
     display: flex;
     flex-direction: column;
-    row-gap: 6px;
+    row-gap: 6px; */
+    position: absolute;
+    top: 102px;
+    right: calc(50% - 617px);
+    z-index: 20;
 `;
+
+const Likegoods = styled.div`
+    padding: 10px;
+    width: 100%;
+    border: 1px solid rgb(204, 204, 204);
+    background: rgb(255, 255, 255);
+    margin-bottom: 6px;
+    `;
+
 
 const SidebarComponent = () => {
     return (
-        
-            <Sidebar>
-                <div>
-                    찜한상품
-                </div>
-                <div>최근본상품</div>
-                <div>TOP</div>
-            </Sidebar>
-
+        <Sidebar>
+            <Likegoods>
+                찜한상품
+            </Likegoods>
+            <div>최근본상품</div>
+            <div>TOP</div>
+        </Sidebar>
     )
 }
 export default SidebarComponent;
