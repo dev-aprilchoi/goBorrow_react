809c3d4bd30fa1fe4316dea70f3f481e24fa0dca
sjkang930 <91238446+sjkang930@users.noreply.github.com>
2023-09-27 15:19:53 +0900

    goods updated

--- src/component/goods.js
+++ src/component/goods.js
@@ -2,6 +2,7 @@ import styled from '@emotion/styled';
 
 const HeadOne = styled.h1`
   margin-bottom: 10px;
+  font-family: 'Courier New', Courier, monospace;
 `;
 
 const Item = styled.div`
@@ -10,7 +11,7 @@ const Item = styled.div`
   //height: 250px;    
   margin-bottom: 5px;
   overflow: hidden;
-  margin-right: 10px;
+  margin-right: 8px;
   margin-bottom: 10px;
   overflow: hidden;
   position: relative;
@@ -43,16 +44,18 @@ const NamePriceDiv = styled.div`
     `;
 
 const ItemName = styled.div`
-  font-size: 17px;
+  font-size: 15px;
   padding-bottom: 20px;
   overflow: hidden;
   text-overflow: ellipsis;
+  font-family: 'Courier New', Courier, monospace;
 `;
 
 const PriceDiv = styled.div`
-  font-size: 17px;
+  font-size: 15px;
   display: flex;
   justify-content: space-between;
+  font-family: 'Courier New', Courier, monospace;
 `;
 
 const ItemPrice = styled.div`
@@ -62,97 +65,98 @@ const ItemPrice = styled.div`
 const ItemTimestamp = styled.div`
   font-size: 14px;
   color: #999;
+  font-family: 'Courier New', Courier, monospace;
   `;
 
 const goodsList = [
     {
-        img: '/goods1.jpeg',
-        description: '체크 셔츠 long product name',
-        price: '39,000원',
+        img: 'goods/goods1.jpeg',
+        description: '소니 카메라 정품',
+        price: '50,000원',
         time: '2시간전',
     },
     {
-        img: '/goods1.jpeg',
-        description: '셔츠',
+        img: 'goods/goods2.jpeg',
+        description: '체크남방',
         price: '29,000원',
         time: '5시간전',
     },
     {
-        img: '/goods1.jpeg',
-        description: '셔츠 product name long sample',
-        price: '29,000원',
+        img: 'goods/goods3.jpeg',
+        description: '소라색 여자니트',
+        price: '20,000원',
         time: '8시간전',
     },
     {
-        img: '/goods1.jpeg',
-        description: '셔츠',
-        price: '29,000원',
+        img: 'goods/goods4.jpeg',
+        description: '반팔 티셔츠',
+        price: '19,000원',
         time: '7시간전',
     },
     {
-        img: '/goods1.jpeg',
-        description: '셔츠',
-        price: '29,000원',
+        img: 'goods/goods5.jpeg',
+        description: '남자 반팔티',
+        price: '21,000원',
         time: '1시간전',
     },
     {
-        img: '/goods1.jpeg',
-        description: '셔츠',
+        img: 'goods/goods6.jpeg',
+        description: '귀여운 장난감',
+        price: '10,000원',
+        time: '2시간전',
+    },
+    {
+        img: 'goods/goods7.jpeg',
+        description: '동물 여자옷',
+        price: '23,000원',
+        time: '2시간전',
+    },
+    {
+        img: 'goods/goods8.jpeg',
+        description: '여자바지 사이즈 26',
         price: '29,000원',
         time: '2시간전',
     },
     {
-        img: '/goods1.jpeg',
-        description: '셔츠',
-        price: '29,000원',
+        img: 'goods/goods9.jpeg',
+        description: '아동한복',
+        price: '39,000원',
         time: '2시간전',
     },
     {
-        img: '/goods1.jpeg',
-        description: '셔츠',
-        price: '29,000원',
-        time: '2시간전',
-    },
-    {
-        img: '/goods1.jpeg',
-        description: '셔츠',
-        price: '29,000원',
-        time: '2시간전',
-    },
-    {
-        img: '/goods1.jpeg',
-        description: '셔츠',
-        price: '29,000원',
+        img: 'goods/goods10.jpeg',
+        description: '동물 파란옷',
+        price: '18,000원',
         time: '20시간전',
     },
     {
-        img: '/goods1.jpeg',
-        description: '셔츠',
-        price: '29,000원',
+        img: 'goods/goods11.jpeg',
+        description: '반스신발 230',
+        price: '20,000원',
         time: '2시간전',
     },
     {
-        img: '/goods1.jpeg',
-        description: '셔츠',
-        price: '29,000원',
+        img: 'goods/goods12.jpeg',
+        description: '컨버스 신발 280',
+        price: '27,000원',
         time: '2시간전',
     },
     {
-        img: '/goods1.jpeg',
-        description: '셔츠',
-        price: '29,000원',
+        img: 'goods/goods13.jpeg',
+        description: '커플가방',
+        price: '30,000원',
         time: '2시간전',
     },
     {
-        img: '/goods1.jpeg',
-        description: '나이키 티셔츠',
-        price: '49,000원',
+        img: 'goods/goods14.jpeg',
+        description: '노란색 유치원가방',
+        price: '12,000원',
         time: '2시간전',
     },
     {
-        img: '/goods1.jpeg',
-        description: '후드티',
-        price: '31,000원',
+        img: 'goods/goods15.jpeg',
+        description: '뽀로로 가방',
+        price: '11,000원',
         time: '23시간전',
     },
 ]
